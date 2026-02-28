05c4a64589d2f2c583894d4ef23e1ca1fd21423b
LocalShare <noreply@localshare.io>
2026-02-27 22:20:55 -0600

    Update Stunl to v5.31.0

--- Formula/stunl.rb
+++ Formula/stunl.rb
@@ -1,16 +1,16 @@
 class Stunl < Formula
   desc "Fast and secure tunneling solution for exposing local services"
   homepage "https://stunl.com"
-  version "5.30.0"
+  version "5.31.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/stunl/cli/releases/download/v5.30.0/stunl-darwin-arm64"
-      sha256 "57c9f98af5a929901c2e153326bcb9e403847efbb92d5ef581c24df8a7794d8b"
+      url "https://github.com/stunl/cli/releases/download/v5.31.0/stunl-darwin-arm64"
+      sha256 "e0fb8ddc5e4a84238d0c7ff7f2f6d3611ba0bacb037b1fd255af018f1db60e17"
     else
-      url "https://github.com/stunl/cli/releases/download/v5.30.0/stunl-darwin-amd64"
-      sha256 "4ea9b048b1c673c0a1996718aa0b2eb49e3c6c2dbe21b0433ae544992d66c5b5"
+      url "https://github.com/stunl/cli/releases/download/v5.31.0/stunl-darwin-amd64"
+      sha256 "4fa2754953452a09c578f00de1c36650ba6008c0615340c7158c1eca5c98f1cf"
     end
   end
 
